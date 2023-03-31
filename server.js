594ab099971d9e324f08d4a2275c32ee63d119e6
GarethWGSMA <110228888+GarethWGSMA@users.noreply.github.com>
2023-03-31 13:17:28 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var handleRequest = function (request, response) {
   response.writeHead(200);
   
-  response.end('Hello from Node Server App deployed on ECS Fargate with CodePipeline!');
+  response.end('Proof - we deploy from a pipeline');
   
 };
 var www = http.createServer(handleRequest);
